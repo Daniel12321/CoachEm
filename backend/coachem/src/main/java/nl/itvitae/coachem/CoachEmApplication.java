ab13006be7eee6078b8273d467f4138a89b0b186
Daniel <danielpeters9797@gmail.com>
2023-03-06 10:57:06 +0100

    Backend initial commit

---
+++ backend/coachem/src/main/java/nl/itvitae/coachem/CoachEmApplication.java
@@ -0,0 +1,13 @@
+package nl.itvitae.coachem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachEmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoachEmApplication.class, args);
+	}
+
+}
