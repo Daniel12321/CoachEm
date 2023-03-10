7fd6a6e33665b377d352b2752a7e30e7040e2a90
Daniel <danielpeters9797@gmail.com>
2023-03-10 10:53:54 +0100

    Finished Pre-backend version

--- frontend/coachem/src/skills/SkillsPage.jsx
+++ frontend/coachem/src/skills/SkillsPage.jsx
@@ -7,34 +7,91 @@ const defCats = [
     { id: 3, name: 'sql', title: 'SQL' },
     { id: 4, name: 'c#', title: 'C#' },
     { id: 5, name: 'python', title: 'Python' },
+    { id: 6, name: 'personal', title: 'Personal' },
 ];
 
 const defSkills = [
-    { id: 1, type: 'hard', name: 'Learn React basics', category: 'javascript' },
+    {
+        id: 1,
+        type: 'hard',
+        name: 'Learn React basics',
+        category: 'javascript',
+        duration: 4,
+    },
     {
         id: 2,
         type: 'hard',
         name: 'Learn React: Advanced',
         category: 'javascript',
+        duration: 5,
     },
     {
         id: 3,
         type: 'hard',
         name: 'Learn React: Expert',
         category: 'javascript',
+        duration: 6,
     },
-    { id: 4, type: 'hard', name: 'Learn Spring Boot', category: 'java' },
-    { id: 5, type: 'hard', name: 'Learn Mapstruct', category: 'java' },
-    { id: 6, type: 'hard', name: 'Learn Java Generics', category: 'java' },
-    { id: 7, type: 'hard', name: 'Learn SQL basics', category: 'sql' },
-    { id: 8, type: 'hard', name: 'Learn SQL: Advanced', category: 'sql' },
-    { id: 9, type: 'hard', name: 'Learn SQL: Expert', category: 'sql' },
-    { id: 10, type: 'hard', name: 'Learn NoSQL', category: 'sql' },
+    {
+        id: 4,
+        type: 'hard',
+        name: 'Learn Spring Boot',
+        category: 'java',
+        duration: 7,
+    },
+    {
+        id: 5,
+        type: 'hard',
+        name: 'Learn Mapstruct',
+        category: 'java',
+        duration: 1,
+    },
+    {
+        id: 6,
+        type: 'hard',
+        name: 'Learn Java Generics',
+        category: 'java',
+        duration: 2,
+    },
+    {
+        id: 7,
+        type: 'hard',
+        name: 'Learn SQL basics',
+        category: 'sql',
+        duration: 4,
+    },
+    {
+        id: 8,
+        type: 'hard',
+        name: 'Learn SQL: Advanced',
+        category: 'sql',
+        duration: 6,
+    },
+    {
+        id: 9,
+        type: 'hard',
+        name: 'Learn SQL: Expert',
+        category: 'sql',
+        duration: 6,
+    },
+    {
+        id: 10,
+        type: 'hard',
+        name: 'Learn NoSQL',
+        category: 'sql',
+        duration: 6,
+    },
+    {
+        id: 11,
+        type: 'soft',
+        name: 'Learn to make personal goals',
+        category: 'personal',
+    },
 ];
 
 export default function SkillsPage() {
-    const [skills, setSkills] = useState(defSkills);
-    const [categories] = useState(defCats);
+    const [skills, setSkills] = useState([]);
+    const [categories, setCategories] = useState([]);
 
     const [name, setName] = useState();
     const [type, setType] = useState();
@@ -43,19 +100,27 @@ export default function SkillsPage() {
     const [maxDuration, setMaxDuration] = useState();
 
     useEffect(() => {
-        fetch('http://localhost:8080/skills')
-            .then((resp) => resp.toJson())
-            .then(setSkills);
+        setSkills(defSkills);
+        setCategories(defCats);
+        // fetch("http://localhost:8080/skills")
+        //   .then((resp) => resp.toJson())
+        //   .then(setSkills);
     }, []);
 
     const filteredSkills = skills
         .filter(
             (s) => !(name && !s.name.toLowerCase().includes(name.toLowerCase()))
         )
-        .filter((s) => !type)
-        .filter((s) => !(category && category !== s.category));
-    // .filter(s => !(minDuration && ))
-    // .filter(s => !(maxDuration && ))
+        .filter((s) => !(type && type !== s.type))
+        .filter((s) => !(category && category !== s.category))
+        .filter(
+            (s) =>
+                !(minDuration && minDuration !== 0 && minDuration > s.duration)
+        )
+        .filter(
+            (s) =>
+                !(maxDuration && maxDuration !== 0 && maxDuration < s.duration)
+        );
 
     return (
         <div className="skills-page">
@@ -100,9 +165,7 @@ const NameFilter = ({ setName }) => (
             type="text"
             name="name"
             id="name"
-            onChange={(e) => {
-                setName(e.target.value);
-            }}
+            onChange={(e) => setName(e.target.value)}
         />
     </div>
 );
@@ -110,10 +173,22 @@ const NameFilter = ({ setName }) => (
 const TypeFilter = ({ setType }) => (
     <div className="skill-filter skill-filter-type">
         <p>Type</p>
-        <input type="radio" name="skill-type" id="soft" value="soft" />
+        <input
+            type="radio"
+            name="skill-type"
+            id="soft"
+            value="soft"
+            onChange={(e) => setType(e.target.value)}
+        />
         <label htmlFor="soft">Soft</label>
         <br />
-        <input type="radio" name="skill-type" id="hard" value="hard" />
+        <input
+            type="radio"
+            name="skill-type"
+            id="hard"
+            value="hard"
+            onChange={(e) => setType(e.target.value)}
+        />
         <label htmlFor="hard">Hard</label>
     </div>
 );
@@ -141,7 +216,7 @@ const CategoryFilter = ({ setCategory, categories }) => (
 
 const DurationFilter = ({ setMinDuration, setMaxDuration }) => (
     <div className="skill-filter skill-filter-duration">
-        <p>Duration (minutes)</p>
+        <p>Duration (hours)</p>
         <label htmlFor="min-duration">Min</label>
         <input
             type="number"
