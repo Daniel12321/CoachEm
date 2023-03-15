853253a9015936f2b28d482f2e96901438d91bf8
Daniel <danielpeters9797@gmail.com>
2023-03-15 11:49:57 +0100

    Added login page

--- frontend/coachem/src/login/LoginPage.jsx
+++ frontend/coachem/src/login/LoginPage.jsx
@@ -1,7 +1,46 @@
+import './LoginPage.css';
 
 export default function LoginPage() {
+    const login = (e) => {
+        e.preventDefault();
+
+        const body = {
+            email: e.target[0].value,
+            password: e.target[1].value,
+        };
+
+        fetch('http://127.0.0.1:8080/api/auth/login', {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: { 'Content-Type': 'application/json' },
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                localStorage.setItem('access_token', data.token);
+                localStorage.setItem('user_role', data.role);
+            });
+    };
 
     return (
-        <h1>Login Page!</h1>
+        <div className="loginpage">
+            <form className="login-form" onSubmit={login}>
+                <h1>Login</h1>
+                <input
+                    type="text"
+                    id="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                />
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    placeholder="Password"
+                    required
+                />
+                <input type="submit" value="Login" />
+            </form>
+        </div>
     );
 }
